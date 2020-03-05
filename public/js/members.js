8deef5796b07466bec73b7591bf0fafa7a22c199
Brayden DeLoach <brayden@deloach.us>
2020-03-05 10:25:41 -0600

    Changed members.js to be able to add a message to the database

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  // $.get("/api/user_data").then(function(data) {
-  //   $(".member-name").text(data.email);
-  // });
+  $.get("/api/user_data").then(function(data) {
+    $(".member-name").text(data.email);
+
+    $("#newMessageSubmitButton").on("click", function(event){
+      event.preventDefault();
+
+      const messageData = {
+        UserId: data.id,
+        message: $("#newMessage").val()
+      };
+
+      $.ajax({
+        url: "/api/messages",
+        type: "POST",
+        data: messageData
+      })
+        .then(function(){
+          window.location.reload();
+        });
+    });
+  });
 });
+
+//newMessage
+//newMessageSubmitButton
