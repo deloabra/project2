593fcfff1ac1315d2c578e35d6c8a8cf9713e463
“Ronny <contact@ronnyperez.com>
2020-03-05 12:58:59 -0600

    merge

--- public/js/members.js
+++ public/js/members.js
@@ -1,14 +1,51 @@
 $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
+    if (!data.goal) {
+      $("#goalBox").html(
+        "<h3>Create a goal to see and update your goal progress</h3>"
+      );
+    } else {
+      $("#goalText").html(
+        `<h3>My Goal: Do ${data.interest} for ${data.goal} ${
+          data.goalUnit === "Distance" ? "Miles" : "Hours"
+        }</h3>`
+      );
+      // progressbar.js@1.0.0 version is used
+      // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-    if(!data.goal){
-      $("#goalBox").html("<h3>Create a goal to see and update your goal progress</h3>");
-    }
-    else{
-      $("#goalText").html(`<h3>My Goal: Do ${data.interest} for ${data.goal} ${data.goalUnit === "Distance"?"Miles":"Hours"}</h3>`);
+      var bar = new ProgressBar.Line(container, {
+        strokeWidth: 4,
+        easing: "easeInOut",
+        duration: 1400,
+        color: "#FFEA82",
+        trailColor: "#eee",
+        trailWidth: 1,
+        svgStyle: { width: "100%", height: "100%" },
+        text: {
+          style: {
+            // Text color.
+            // Default: same as stroke color (options.color)
+            color: "#999",
+            position: "absolute",
+            right: "0",
+            top: "30px",
+            padding: 0,
+            margin: 0,
+            transform: null
+          },
+          autoStyleContainer: false
+        },
+        from: { color: "#FFEA82" },
+        to: { color: "#ED6A5A" },
+        step: (state, bar) => {
+          bar.setText(Math.round(bar.value() * 100) + " %");
+        }
+      });
+
+      bar.animate(1.0); // Number from 0.0 to 1.0
     }
 
-    $("#newMessageSubmitButton").on("click", function(event){
+    $("#newMessageSubmitButton").on("click", function(event) {
       event.preventDefault();
 
       const messageData = {
@@ -20,13 +57,12 @@ $(document).ready(function() {
         url: "/api/messages",
         type: "POST",
         data: messageData
-      })
-        .then(function(){
-          window.location.reload();
-        });
+      }).then(function() {
+        window.location.reload();
+      });
     });
 
-    $("#updateGoalButton").on("click", function(event){
+    $("#updateGoalButton").on("click", function(event) {
       event.preventDefault();
 
       const progressData = {
@@ -37,10 +73,9 @@ $(document).ready(function() {
         url: "/api/goalUpdate",
         type: "PATCH",
         data: progressData
-      })
-        .then(function(){
-          window.location.reload();
-        });
+      }).then(function() {
+        window.location.reload();
+      });
     });
   });
 });
