c6a3696aee07503226956c21aba8b955d4a88e37
Brayden DeLoach <brayden@deloach.us>
2020-03-05 09:24:49 -0600

    Made messages route do an sql query to get info for handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,8 @@
 // Requiring path to so we can use relative routes to our HTML files
 // var path = require("path");
 
+var db = require("../models");
+
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
@@ -40,7 +42,10 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.render("members", {object:req});
+    db.sequelize.query("select messages.message, Users.email, Users.name FROM messages INNER JOIN Users on messages.UserId = Users.id")
+      .then(function(results){
+        console.log(results[0]);
+        res.render("members", {messages:results[0]});
+      });
   });
-
 };
